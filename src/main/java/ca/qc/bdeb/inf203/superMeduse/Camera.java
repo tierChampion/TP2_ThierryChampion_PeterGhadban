551d7f2902541b5af56ef316fc51d67cfd1556c8
thier <thierrychamp15@gmail.com>
2021-11-12 10:15:35 -0500

    Super méduse!

--- src/main/java/ca/qc/bdeb/inf203/superMeduse/Camera.java
+++ src/main/java/ca/qc/bdeb/inf203/superMeduse/Camera.java
@@ -1,4 +1,27 @@
 package ca.qc.bdeb.inf203.superMeduse;
 
 public class Camera {
+
+    private double x, y;
+    private double vy, ay;
+
+    public Camera(double x, double y, double vy, double ay) {
+        this.x = x;
+        this.y = y;
+        this.vy = vy;
+        this.ay = ay;
+    }
+
+    public void update(double deltaTime) {
+        vy += ay * deltaTime;
+        y += vy * deltaTime;
+    }
+
+    public double getScreenX(double worldX) {
+        return worldX - x;
+    }
+
+    public double getScreenY(double worldY) {
+        return worldY - y;
+    }
 }
