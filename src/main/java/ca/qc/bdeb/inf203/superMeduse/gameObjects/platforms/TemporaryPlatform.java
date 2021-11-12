dc278b51ca0957897c09f4f6ad6acbd21f344b33
thier <thierrychamp15@gmail.com>
2021-11-12 11:48:41 -0500

    Super méduse!

--- src/main/java/ca/qc/bdeb/inf203/superMeduse/gameObjects/platforms/TemporaryPlatform.java
+++ src/main/java/ca/qc/bdeb/inf203/superMeduse/gameObjects/platforms/TemporaryPlatform.java
@@ -4,6 +4,11 @@ import ca.qc.bdeb.inf203.superMeduse.gameObjects.Jellyfish;
 import javafx.scene.paint.Color;
 
 public class TemporaryPlatform extends Platform{
+
+    private boolean lastUsed = false; // is stepping on plat
+    private boolean used = false; // ever stepped on plat
+    private boolean dead = false; // jumped off plat
+
     public TemporaryPlatform(double x, double y,
                              double vx, double vy,
                              double ax, double ay,
@@ -11,7 +16,24 @@ public class TemporaryPlatform extends Platform{
                              double maxX){
         super(x, y, vx, vy, ay, ax, w, h, maxX, Color.BLACK);
     }
-    public void effect(Jellyfish jelly){
 
+    @Override
+    public void update(double deltaTime) {
+
+        if (used && !lastUsed) {
+            dead = true;
+            vy = 200;
+        }
+        if (dead) {
+            super.update(deltaTime);
+        }
+        lastUsed = false;
+    }
+
+    public void effect(Jellyfish jelly){
+        lastUsed = true;
+        used = true;
+        super.effect(jelly);
     }
 }
+
